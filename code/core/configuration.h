4cfc57f6be1205996f6cdb4902441ef500e428de
Al_Sah <aplexidr@gmail.com>
2021-05-14 23:58:34 +0300

    Added Cmake configuration in "core"

---
+++ code/core/configuration.h
@@ -0,0 +1,13 @@
+#ifndef BASIC_CONFIGURATION
+#undef BASIC_CONFIGURATION
+
+//#define lambdas
+//#define headers_includes
+
+//#define BOT
+//#define CONTROL
+#include "cmake_configuration.h"
+
+#endif //BASIC_CONFIGURATION
+
+
